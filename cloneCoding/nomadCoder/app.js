907daf447a7436b58fe71ea6f78b2d4e4d7212aa
ryukyunghye <ryukyung123@naver.com>
2022-11-22 16:16:13 +0900

    :sparkles: nomadcoder challenge day2

--- cloneCoding/nomadCoder/app.js
+++ cloneCoding/nomadCoder/app.js
@@ -1,1 +1,42 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "gaori";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hi " + myName);
+myName = "AkO";
+console.log("My new name is " + myName);
+
+const testNull = null;
+let something;
+console.log(something, testNull);
+
+// array
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+const variableWeek = [mon, tue, wed, thu, fri, sat, sun];
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+console.log(daysOfWeek);
+console.log(daysOfWeek[4]);
+
+// object
+const playerName = "gaori";
+const playerPoints = 3000;
+const playerLogin = false;
+
+const player = {
+  name: "gaori",
+  points: 3000,
+  login: false,
+};
+console.log(player);
+console.log(playerName, player.name);
+player.name = "Ako";
+console.log(playerName, player.name);
